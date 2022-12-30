eb141e0d51c60d21f7d9477dafb2dc3644fd01e4
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-12-30 20:15:21 -0200

    Create styles.ts

---
+++ src/screens/GetStarted/styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`
+
+export const Title = styled.Text`
+  font-size: 32px;
+  font-weight: bold;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  color: #4C956C;
+`
+
+export const Button = styled.TouchableOpacity`
+  padding: 16px 36px;
+  color: #FFF;
+  background: #4C956C;
+  border-radius: 8px; 
+  text-align: center;
+  font-weight: bold;
+  margin-top: 16px;
+`
