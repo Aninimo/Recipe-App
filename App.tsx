b57e945fed24a927a0cd4b17ae7c5eac87fe9bd4
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-12-30 16:42:17 -0200

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { AppContext } from './context'
+
+import { Routes } from './routes'
+
+export default function App() {
+  return(
+    <AppContext>
+      <Routes/>
+    </AppContext>
+  )
+}
