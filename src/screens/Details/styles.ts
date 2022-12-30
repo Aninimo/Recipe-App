58434f70434e692b42c3e1a9cbb399288f53474f
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-12-30 19:57:38 -0200

    Create styles.ts

---
+++ src/screens/Details/styles.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.ScrollView`
+  background: #EFF1ED;
+`
+
+export const Name = styled.Text`
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  font-size: 24px;
+  font-weight: bold;
+`
+
+export const ContainerInfo = styled.View`
+  padding: 16px;
+`
+
+export const Subtitle = styled.Text`
+  font-weight: bold;
+  margin-bottom: 5px;
+`
+
+export const ContainerIngredient = styled.ScrollView`
+
+`
+
+export const Button = styled.TouchableOpacity`
+  padding: 1rem 3rem;
+  color: #FFF;
+  background: #4C956C;
+  border-radius: 8px; 
+  text-align: center;
+  font-weight: bold;
+  margin-top: 16px;
+`
+
+export const Instruction = styled.Text`
+  margin-top: 5px;
+`
+
+export const Button = styled.TouchableOpacity`
+  padding: 1rem 3rem;
+  color: #FFF;
+  background: #4C956C;
+  border-radius: 8px; 
+  text-align: center;
+  font-weight: bold;
+  margin-top: 16px;
+`
